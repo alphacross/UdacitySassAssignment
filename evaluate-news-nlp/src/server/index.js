b4eb3f6318dbc39b5739f6160bbcdf71c49ba481
Andreas Henggie <andreas.henggie@bertelsmann.de>
2023-06-22 18:55:54 +0800

    Setup API

--- evaluate-news-nlp/src/server/index.js
+++ evaluate-news-nlp/src/server/index.js
@@ -1,23 +1,41 @@
+const dotenv = require('dotenv');
+dotenv.config();
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
 app.use(express.static('dist'))
+app.use(cors());
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+app.post('/test', async function (req, res) {
+    let formData = new FormData();
+
+    formData.append('key', process.env.API_KEY);
+    formData.append('lang', 'auto');
+    formData.append('txt', req.body.txt);
+    formData.append('of', 'json');
+
+    const response = await fetch('https://api.meaningcloud.com/sentiment-2.1', {
+        method: 'POST',
+        body: formData
+    });
+    res.send(await response.json());
 })
+
--- evaluate-news-nlp/webpack.prod.js
+++ evaluate-news-nlp/webpack.prod.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 module.exports = {
     entry: './src/client/index.js',
@@ -11,6 +14,10 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
@@ -18,6 +25,18 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        // new WorkboxPlugin.GenerateSW({
+        //     swDest: './dist/sw.js'
+        // })
     ]
 }
